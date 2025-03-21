115714bde574853511dab96a7a561164cfa8f81c
Daniel Paggiola <dpaggiola@gmail.com>
2025-03-21 16:02:59 -0300

    Clase 17

--- java_jdbc/src/main/java/apps/desarrollo1/EjemploJdbcDelete.java
+++ java_jdbc/src/main/java/apps/desarrollo1/EjemploJdbcDelete.java
@@ -5,10 +5,10 @@ import apps.desarrollo1.repositorio.ProductoRepositorioImpl;
 import apps.desarrollo1.repositorio.Repositorio;
 import apps.desarrollo1.util.ConexionBaseDatos;
 
-import java.sql.*;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-public class EjemploJdbc {
+public class EjemploJdbcDelete {
     public static void main(String[] args) {
 
         try (Connection conn = ConexionBaseDatos.getInstance()) {
@@ -19,13 +19,9 @@ public class EjemploJdbc {
             System.out.println("=========== obtener por id ===========");
             System.out.println(repositorio.porId(2L));
 
-            System.out.println("=========== insertar nuevo producto ===========");
-            Producto producto = new Producto();
-            producto.setNombre("Teclado mecánico");
-            producto.setPrecio(500);
-            producto.setFechaRegistro(new Date());
-            repositorio.guardar(producto);
-            System.out.println("Producto guardado con éxito");
+            System.out.println("=========== Eliminar producto ===========");
+            repositorio.eliminar(3L);
+            System.out.println("Producto eliminado con éxito");
             repositorio.listar().forEach(System.out::println);
 
         } catch (SQLException e) {
--- java_jdbc/src/main/java/apps/desarrollo1/repositorio/ProductoRepositorioImpl.java
+++ java_jdbc/src/main/java/apps/desarrollo1/repositorio/ProductoRepositorioImpl.java
@@ -35,11 +35,11 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
         try (PreparedStatement stmt = getConnection().
                 prepareStatement("SELECT * FROM productos WHERE id = ?")) {
             stmt.setLong(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                producto = crearProducto(rs);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    producto = crearProducto(rs);
+                }
             }
-            rs.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -48,12 +48,36 @@ public class ProductoRepositorioImpl implements Repositorio<Producto> {
 
     @Override
     public void guardar(Producto producto) {
+        String sql = null;
+        if (producto.getId() != null && producto.getId() > 0) {
+            sql = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre, precio, fecha_registro) VALUES (?,?,?)";
+        }
+        try (PreparedStatement stmt = getConnection().prepareStatement(sql)){
+            stmt.setString(1, producto.getNombre());
+            stmt.setLong(2, producto.getPrecio());
 
+            if (producto.getId() != null && producto.getId() > 0) {
+                stmt.setLong(3, producto.getId());
+            } else {
+                stmt.setDate(3, new Date(producto.getFechaRegistro().getTime()));
+            }
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void eliminar(Long id) {
-
+        try (PreparedStatement stmt = getConnection().prepareStatement("DELETE FROM productos WHERE id=?")){
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static Producto crearProducto(ResultSet rs) throws SQLException {
